cae509db68f0591d04a5cbeb59816aef9388a69f
Agarwalkhushi19 <khushiagarwal0410@gmail.com>
2024-12-19 14:36:37 +0530

    Gallery Templates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Gallery from './components/Gallery';
 import './App.css';
-
+import {BrowserRouter as Router} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <Router>
+
+      <Gallery />
+    </Router>
+     
+    
   );
 }
 
